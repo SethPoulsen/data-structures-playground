07ec55f80735faed11368f09a62477ce69b49196
Seth Poulsen <poulsenseth@yahoo.com>
2020-04-28 16:23:18 -0500

    Clean up webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -6,37 +6,20 @@ module.exports = {
     },
 
     mode: "development",
-    // Enable sourcemaps for debugging webpack's output.
     devtool: "source-map",
 
     resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
         extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
-        // alias: {
-        //     "fabric": path.resolve(__dirname, './node_modules/fabric/dist/fabric.js')
-        // }
     },
 
-    // externals : {
-    //     "fabric" : "require('fabric')"
-    // },
     module: {
         rules: [
             // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
             { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
             { test: /\.js$/, loader: "source-map-loader" }
         ],
-
-        // preLoaders: [
-        //     // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-        // ]
     },
 
     watch: true, 
-    // watchOptions: {
-    //     aggregateTimeout: 200,
-    //     poll: 1000
-    // }
-
-    // Other options...
 };
