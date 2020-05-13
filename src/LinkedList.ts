23e29e25e30d4b0a3ba315d746897e2b80d29a19
jasonxia17 <jasonx3@illinois.edu>
2020-05-13 09:37:08 -0500

    Lock movement of circles and text

--- src/LinkedList.ts
+++ src/LinkedList.ts
@@ -8,11 +8,11 @@ export class LinkedList {
     private head: Node = null;
     private canvas: fabric.Canvas;
 
-    constructor(canvasEl: HTMLCanvasElement, values:number[]=[]) {
+    constructor(canvasEl: HTMLCanvasElement, values: number[] = []) {
 
         this.canvas = new fabric.Canvas(canvasEl);
         this.canvas.selection = false;
-        
+
         // this allows us to draw circles using the coordinates of their centers
         fabric.Object.prototype.originX = fabric.Object.prototype.originY = 'center';
 
@@ -22,28 +22,37 @@ export class LinkedList {
         }
 
         this.draw()
-        
+
         let nul = new fabric.IText('NULL', {
             left: Config.LIST_X + (values.length * Config.NODE_SPACE),
             top: Config.LIST_Y + (Config.NODE_SIZE / 2),
             fill: '#black',
         });
-        this.canvas.add(nul) 
+        this.canvas.add(nul)
 
         let head = new fabric.IText('head', {
             left: 20,
             top: 20,
             fill: '#black',
         });
-        this.canvas.add(head) 
+        this.canvas.add(head)
 
         let line = makeLine([60, 60, 100, 140]);
         let arrow1 = makeLine([95, 110, 100, 140]);
         let arrow2 = makeLine([70, 120, 100, 140]);
         this.canvas.add(line);
-        this.canvas.add(arrow1); 
+        this.canvas.add(arrow1);
         this.canvas.add(arrow2);
-    
+
+        const self = this;
+        this.canvas.on('object:moving', function (e) {
+            let temp = self.head;
+            while (temp) {
+                temp.redraw();
+                temp = temp.next;
+            }
+        });
+
     }
 
     public draw() {
@@ -51,7 +60,7 @@ export class LinkedList {
         let temp = this.head;
         while (temp) {
             temp.draw(this.canvas, leftEdge);
-            
+
             leftEdge += Config.NODE_SPACE;
             temp = temp.next;
         }
--- src/Node.ts
+++ src/Node.ts
@@ -7,39 +7,47 @@ export class Node {
     public data: number;
     public next: Node;
     private representation: fabric.Object;
-    
+
+    private canvasObjects: {
+        circle: fabric.Circle;
+        text: fabric.Text;
+    };
+
     constructor(data: number, next: Node) {
         this.data = data;
         this.next = next;
     }
-    
+
     public draw(canvas: fabric.Canvas, leftEdge: number): void {
         // TODO: figures out how to use fabric.js to group the circle, 
         // number, and arrow together so that they all move together 
         // and can be treated as a single object to store in this.representation 
 
         // Draw the Node
-        var circle = new fabric.Circle({
-            radius: Config.NODE_SIZE, 
-            fill: '#00000000', 
-            left: leftEdge, 
-            top: Config.LIST_Y, 
-            stroke: 'black',
-            strokeWidth: 2,
-            hasControls: false,
-            hasBorders: false,
-            hoverCursor: "grab",
-            moveCursor: "grabbing",
-        });
+        this.canvasObjects = {
+            circle: new fabric.Circle({
+                radius: Config.NODE_SIZE,
+                fill: '#00000000',
+                left: leftEdge,
+                top: Config.LIST_Y,
+                stroke: 'black',
+                strokeWidth: 2,
+                hasControls: false,
+                hasBorders: false,
+                hoverCursor: "grab",
+                moveCursor: "grabbing",
+            }),
 
-        let num = new fabric.IText(this.data.toString(), {
-            left: leftEdge,
-            top: Config.LIST_Y,
-            fill: '#black',
-            evented: false,
-            selectable: false
-        });
-        canvas.add(circle, num);
+            text: new fabric.IText(this.data.toString(), {
+                left: leftEdge,
+                top: Config.LIST_Y,
+                fill: '#black',
+                evented: false,
+                selectable: false
+            })
+        };
+
+        canvas.add(this.canvasObjects.circle, this.canvasObjects.text);
 
         // Draw the next pointer
         // TODO: write a utility function that will draw an arrow given start and end points
@@ -49,9 +57,15 @@ export class Node {
         var arrow1 = makeLine([nextNodeLeft - 20, nodeCenter + 20, nextNodeLeft, nodeCenter]);
         let arrow2 = makeLine([nextNodeLeft - 20, nodeCenter - 20, nextNodeLeft, nodeCenter]);
         canvas.add(line);
-        canvas.add(arrow1); 
+        canvas.add(arrow1);
         canvas.add(arrow2);
+    }
 
+    public redraw(): void {
+        this.canvasObjects.text.set({
+            left: this.canvasObjects.circle.left,
+            top: this.canvasObjects.circle.top,
+        });
     }
 
 }
