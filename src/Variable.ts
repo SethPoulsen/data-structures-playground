f0f27cc54f7f2af9d9ed53a04c9a537d8b6e89c0
jasonxia17 <jasonx3@illinois.edu>
2020-05-13 16:05:51 -0500

    Calculate variable's contact point

--- src/Variable.ts
+++ src/Variable.ts
@@ -40,9 +40,16 @@ export class Variable {
      * @param angle the angle at which the pointer will be drawn, in radians.
      */
     public getContactPoint(angle: number) {
+        // r is distance from center to contact point
+        // take minimum to determine if it intersects the vertical or horizontal boundary first
+        const r = Math.min(
+            Math.abs((this.representation.width / 2) / Math.cos(angle)),
+            Math.abs((this.representation.height / 2) / Math.sin(angle))
+        );
+
         return {
-            x: this.representation.left + Math.cos(angle) * Config.NODE_SIZE,
-            y: this.representation.top + Math.sin(angle) * Config.NODE_SIZE,
+            x: this.representation.left + Math.cos(angle) * r,
+            y: this.representation.top + Math.sin(angle) * r
         }
     }
 
